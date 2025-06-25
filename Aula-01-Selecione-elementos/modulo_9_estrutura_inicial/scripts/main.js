a4e45fea23d891c41814c382f5ff403fa9024b21
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-06-24 21:10:15 -0400

    Hide form by default and add slide toggle actions
    Set the form to be hidden by default using CSS. Updated JavaScript to slide the form down when the header button is clicked and slide it up when the cancel button is clicked, improving the user interaction flow.

--- Aula-01-Selecione-elementos/modulo_9_estrutura_inicial/scripts/main.js
+++ Aula-01-Selecione-elementos/modulo_9_estrutura_inicial/scripts/main.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
-    console.log(document.querySelector('header button'))
-    console.log($('#botao-cancelar'))
-
-
-    document.querySelector('header button').addEventListener('click', function() {
+    $('header button').click(function() {
+        $('form').slideDown()
 
     })
 
-    $('header button').click(function() {
-        alert('Expandir Fomulário')
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp()
     })
 
     $('form').on('submit', function(e) {
