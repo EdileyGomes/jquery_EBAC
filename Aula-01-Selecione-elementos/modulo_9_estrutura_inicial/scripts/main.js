a4c4af3ebb7ea7a542060608a6748152a0f8e8fd
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-06-24 21:03:41 -0400

    Add event handlers for form and header button
    Added click event listeners to the header button using both vanilla JS and jQuery. Implemented an alert on button click and prevented default form submission while logging a message.

--- Aula-01-Selecione-elementos/modulo_9_estrutura_inicial/scripts/main.js
+++ Aula-01-Selecione-elementos/modulo_9_estrutura_inicial/scripts/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
     console.log(document.querySelector('header button'))
     console.log($('#botao-cancelar'))
+
+
+    document.querySelector('header button').addEventListener('click', function() {
+
+    })
+
+    $('header button').click(function() {
+        alert('Expandir Fomulário')
+    })
+
+    $('form').on('submit', function(e) {
+        console.log('submit')
+        e.preventDefault()
+    })
 })
