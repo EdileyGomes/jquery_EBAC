66e00034324cf42d7d57c59c1713aea74f16f7c0
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-06-24 21:20:53 -0400

    Update main.js

--- Aula-01-Selecione-elementos/modulo_9_estrutura_inicial/scripts/main.js
+++ Aula-01-Selecione-elementos/modulo_9_estrutura_inicial/scripts/main.js
@@ -1,7 +1,6 @@
-$(document).ready(function() {
+    $(document).ready(function() {
     $('header button').click(function() {
         $('form').slideDown()
-
     })
 
     $('#botao-cancelar').click(function(){
