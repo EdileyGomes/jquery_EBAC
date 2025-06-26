8a18a50f85714e9b19ddeafe4dfcc4fb5b0e21f9
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-06-25 20:48:23 -0400

    Enable dynamic image addition to photo gallery
    Added functionality to allow users to insert new images into the gallery via a form. Updated the input field with an ID for easier selection, improved overlay visibility on hover, and implemented the form submission logic in JavaScript to append new images to the gallery.

--- Aula-02-Manipule-eventos/scripts/main.js
+++ Aula-02-Manipule-eventos/scripts/main.js
@@ -1,10 +1,26 @@
 $(document).ready(function () {
 
-    $('header button').click(function() {
+    $('header button').click(function () {
         $('form').slideDown()
     })
 
-    $('#botao-cancelar').on('click', function() {
+    $('#botao-cancelar').on('click', function () {
         $('form').slideUp()
+    })
+
+    $('form').on('submit', function (e) {
+        e.preventDefault()
+        const enderecoDaNovaImagem = $('#endereço-imagem-nova').val()
+        const novoItem = $('<li></li>')
+
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem)
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="ver imagem em tamanho real">
+                Ver Imagem em Tamanho Real
+                </a>
+            </div>
+            `).appendTo(novoItem);
+            $( novoItem ).appendTo('ul');
     })
 })
